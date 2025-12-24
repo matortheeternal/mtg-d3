88bbe6741ccb736516dfc3e683c9a90a04d153dd
Mator <matortheeternal@users.noreply.github.com>
2025-12-23 20:42:39 -0800

    uppercase first letter of rarity

--- src/cardProperties.js
+++ src/cardProperties.js
@@ -17,10 +17,10 @@ const colorStyles = {
 };
 
 const rarityStyles = {
-    common: { g1: 'black', g2: 'black', g3: 'black', g4: 'black', color: '#FFF' },
-    uncommon: { g1: '#6c7d8f', g2: '#c0d6e5', g3: '#c0d6e5', g4: '#9ab4c8', color: '#000' },
-    rare: { g1: '#876c13', g2: '#f6e8a3', g3: '#f6e8a3', g4: '#d7bf55', color: '#000' },
-    mythic: { g1: '#b02929', g2: '#ffb347', g3: '#ffb347', g4: '#ff8a00', color: '#000' }
+    Common: { g1: 'black', g2: 'black', g3: 'black', g4: 'black', color: '#FFF' },
+    Uncommon: { g1: '#6c7d8f', g2: '#c0d6e5', g3: '#c0d6e5', g4: '#9ab4c8', color: '#000' },
+    Rare: { g1: '#876c13', g2: '#f6e8a3', g3: '#f6e8a3', g4: '#d7bf55', color: '#000' },
+    Mythic: { g1: '#b02929', g2: '#ffb347', g3: '#ffb347', g4: '#ff8a00', color: '#000' }
 };
 
 const typePriority = [
@@ -61,7 +61,9 @@ export const SubType = {
 
 export const Rarity = {
     key: 'Rarity',
-    getValue: card => card.rarity,
+    getValue(card) {
+        return card.rarity.slice(0, 1).toUpperCase() + card.rarity.slice(1);
+    },
     style(value) {
         const s = rarityStyles[value] || rarityStyles.common;
         return {
